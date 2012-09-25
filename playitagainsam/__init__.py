054e4c393efc88d8e44d203f022d038b4966aa06
Ryan Kelly <ryan@rfk.id.au>
2012-09-26 00:17:03 +1000

    Prep v0.2.1

--- playitagainsam/__init__.py
+++ playitagainsam/__init__.py
@@ -105,7 +105,7 @@ that you should be aware of:
 
 __ver_major__ = 0
 __ver_minor__ = 2
-__ver_patch__ = 0
+__ver_patch__ = 1
 __ver_sub__ = ""
 __ver_tuple__ = (__ver_major__, __ver_minor__, __ver_patch__, __ver_sub__)
 
